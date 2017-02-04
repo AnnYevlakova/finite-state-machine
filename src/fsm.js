ebc25a06419b1fb0cb2e6c21e0a561223ad48b7e
AnnYevlakova <ann.yevlakova@gmail.com>
2017-02-04 22:37:42 +0300

    Add files via upload
    built class FSM and his methods

--- src/fsm.js
+++ src/fsm.js
@@ -1,59 +1,101 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
+    constructor(config) {
+        if (config == undefined) {
+        throw new Error("arguments are necessary");
+     }
+        this.config = config;
+		this.initial = 'normal';
+        this.prevState = null;
+		this.stackRedo = [];
+		this.stackUndo = [];
+    }
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+    getState() {
+        return this.initial;
+    }
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+    changeState(state) {
+		if(state == undefined) {
+            throw new Error("arguments are necessary");
+        }
+        if(state == 'normal' || state == 'busy' || state == 'hungry' || state == 'sleeping'){
+            this.prevState = this.initial;
+            this.initial = state;
+			this.stackUndo.push(this.prevState);
+			this.stackRedo = [];
+        } else {
+            throw new Error("does not exist");
+        }
+    }
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+    trigger(event) {
+        if (event == undefined) {
+            throw new Error("arguments are necessary");
+        }
+		if(this.config.states[this.initial].transitions[event] != null) {
+            this.prevState = this.initial;
+            this.initial = this.config.states[this.initial].transitions[event];
+			this.stackUndo.push(this.prevState);
+			this.stackRedo = [];
+        } else {
+            throw new Error("does not exist");
+        }
+    }
+    
+    reset() {
+        this.prevState = null;
+        this.initial = this.config.initial;
+		this.canRedo = false;
+		this.stackRedo = [];
+		this.stackUndo = [];
+    }
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+    getStates(event) {
+		if (event == undefined) {
+            return ['normal', 'busy', 'hungry', 'sleeping'];
+        }
+		var states = [];
+        for(var key in this.config.states) {
+            if( event in this.config.states[key].transitions) {
+                states.push(key);
+            }
+		}
+		 
+		return states;
+		
+    }
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+    undo() {
+        if(this.stackUndo.length != 0) {
+			var state = this.initial;
+			this.stackRedo.push(state);
+			this.initial = this.prevState; 
+            this.prevState = state;
+			this.stackUndo.pop();
+			return true;
+		} else {
+			return false;
+		}
+    }
+        
+    redo() {
+        if(this.stackRedo.length != 0) {
+			var state = this.initial;
+			this.stackUndo.push(state);
+			this.initial = this.prevState; 
+            this.prevState = state;
+			this.stackRedo.pop();
+			return true;
+		} else {
+			return false;
+		}
+    }
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
-
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
-
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+    clearHistory() {
+		this.prevState = null;
+		this.stackRedo = [];
+		this.stackUndo = [];
+    }
 }
 
 module.exports = FSM;
